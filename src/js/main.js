a3eac0882b9f843602bf5368411d5126a919db59
sergey <sergey.martynuk@gmail.com>
2016-01-21 20:43:14 +0200

    new commit

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by martynuk on 21.09.15.
+ */
+/*
+ * Third party
+ */
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.min.js
+
+
+/*
+ * Custom main
+ */
+
+//= partials/common.js
+
+
+
+
+
+
+
+
+
+
+
